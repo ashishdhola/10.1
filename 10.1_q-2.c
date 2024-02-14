68c32fe89fe3d10775adc08db779e55fba18c2ce
Ashish Dhola <89638645+ashishdhola@users.noreply.github.com>
2024-02-14 16:24:45 +0530

    Add files via upload

---
+++ 10.1_q-2.c
@@ -0,0 +1,24 @@
+// Q.2 Write a Program to check if a given number is divisible by both 3 & 5 or not using UDF.For example,
+//TNRS
+
+#include<stdio.h>
+
+ans(int x)
+{
+	
+	if ((x % 3 == 0) && (x % 5 == 0))
+	 {
+        printf("Number is divisible by 3 and 5");
+    }
+	 else
+	  {
+        printf("Number is not divisible by 3 and 5");
+    }	
+}
+void main()
+{	
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    ans(a);
+}
